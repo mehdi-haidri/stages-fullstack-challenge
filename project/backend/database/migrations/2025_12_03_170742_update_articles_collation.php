68bc39d31c5522bc84c92ff554c5fdd9047ea449
mehdi-haidri <mehdihaidri3@gmail.com>
2025-12-04 02:25:50 +0100

    fix(collation): make search insensitive to accents by updating article collation to utf8mb4_unicode_ci [BUG-001]

---
+++ project/backend/database/migrations/2025_12_03_170742_update_articles_collation.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateArticlesCollation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+            // 1. Définir le charset et la collation corrects pour 'title'
+            DB::statement('ALTER TABLE articles MODIFY title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+}
