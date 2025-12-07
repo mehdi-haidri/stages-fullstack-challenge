f4260bbf92334596d7ff91a6c536139574a0e471
mehdi-haidri <mehdihaidri3@gmail.com>
2025-12-07 04:37:21 +0100

    fix(docker): Ensure storage symlink creation succeeds at runtime

--- project/backend/entrypoint.sh
+++ project/backend/entrypoint.sh
@@ -4,9 +4,6 @@
 # 1. Wait for the file system to be fully mounted (optional but safer)
 sleep 1
 
-# 2. Delete any previous broken link that might have been mounted from the host.
-rm -f /var/www/html/public/storage
-
 # 3. Create the correct symbolic link *at runtime*.
 # The correct link must be relative or absolute internal path.
 ln -s /var/www/html/storage/app/public /var/www/html/public/storage
